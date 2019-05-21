05ec9e0b5564d9bb53fc345102e4320c25236cb6
Ali Hacks <alihacks>
2019-05-20 20:48:37 -0400

    e2e support

--- project/meandemo/e2e/src/app.e2e-spec.ts
+++ project/meandemo/e2e/src/app.e2e-spec.ts
@@ -3,12 +3,12 @@ import { AppPage } from './app.po';
 describe('workspace-project App', () => {
   let page: AppPage;
 
-  beforeEach(() => {
+  beforeEach(async () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  it('should display welcome message', async () => {
+    await page.navigateTo();
+    expect(await page.getParagraphText()).toEqual('One MEAN CRUD on Docker Demo');
   });
 });
--- project/meandemo/e2e/src/app.po.ts
+++ project/meandemo/e2e/src/app.po.ts
@@ -1,11 +1,11 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo() {
-    return browser.get('/');
+  async navigateTo() {
+    await browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  async getParagraphText() {
+    return element(by.css('h1')).getText();
   }
 }
