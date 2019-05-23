a1ce5eecec5e18355a7409fcf5f5a7c1525c0b48
mseller4 <mike.sellers@jhu.edu>
2019-05-22 21:04:26 -0400

    Updated protractor tests and updated develoipment compose yml.

--- project/meandemo/e2e/src/app.e2e-spec.ts
+++ project/meandemo/e2e/src/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import { browser, by, element } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -7,8 +8,16 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', async () => {
+  it('It should display the title welcome message.', async () => {
     await page.navigateTo();
     expect(await page.getParagraphText()).toEqual('One MEAN CRUD on Docker Demo');
   });
+
+  it('It should allow form data entry.', async () => {
+    await page.navigateTo();
+    await page.createEntry();
+    await page.editEntry();
+
+  });
+
 });
--- project/meandemo/e2e/src/app.po.ts
+++ project/meandemo/e2e/src/app.po.ts
@@ -1,4 +1,4 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, protractor } from 'protractor';
 
 export class AppPage {
   async navigateTo() {
@@ -8,4 +8,37 @@ export class AppPage {
   async getParagraphText() {
     return element(by.css('h1')).getText();
   }
+
+  async createEntry() {
+    var EC = protractor.ExpectedConditions;
+    await browser.wait(EC.presenceOf(element(by.id('addEntryLink'))), 5000, 'Element taking too long to appear in the DOM');
+    element(by.id('addEntryLink')).click();  
+    await browser.wait(EC.presenceOf(element(by.css("input[formControlName=URL]"))), 5000, 'Element taking too long to appear in the DOM');
+    await element(by.css("input[formControlName=URL]")).sendKeys('https://github.com/jhuopensource/meandemo');
+    await element(by.css("input[formControlName=title]")).sendKeys('MEAN Demo');
+    await element(by.css("input[formControlName=author]")).sendKeys('Mike Sellers and Ali Soylu');
+    await element(by.css("textarea[formControlName=description]")).sendKeys('One MEAN (Mongo, Express, Angular, Node.js) CRUD on Docker Demo App.');
+    await element(by.id('save')).click();  
+    await element(by.id('delete')).click();
+    return;
+  }
+
+  async editEntry() {
+    var EC = protractor.ExpectedConditions;
+    await browser.wait(EC.presenceOf(element(by.id('addEntryLink'))), 5000, 'Element taking too long to appear in the DOM');
+    element(by.id('addEntryLink')).click();  
+    await browser.wait(EC.presenceOf(element(by.css("input[formControlName=URL]"))), 5000, 'Element taking too long to appear in the DOM');
+    await element(by.css("input[formControlName=URL]")).sendKeys('https://github.com/jhuopensource/meandemo');
+    await element(by.css("input[formControlName=title]")).sendKeys('MEAN Demo');
+    await element(by.css("input[formControlName=author]")).sendKeys('Mike Sellers and Ali Soylu');
+    await element(by.css("textarea[formControlName=description]")).sendKeys('One MEAN (Mongo, Express, Angular, Node.js) CRUD on Docker Demo App.');
+    await element(by.id('save')).click();  
+    await element(by.id('edit')).click();
+    await element(by.css("textarea[formControlName=description]")).sendKeys(' Was just CI/CD Pipieline Integrated.');
+    await element(by.id('save')).click();  
+    //await element(by.id('delete')).click();
+    return;
+  }
+
+  
 }
