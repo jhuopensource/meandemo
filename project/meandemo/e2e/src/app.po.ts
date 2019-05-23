f0dd84146a413ba4ff64caa1318f1cf504f1a895
mseller4 <mike.sellers@jhu.edu>
2019-05-22 21:42:54 -0400

    Linting

--- project/meandemo/e2e/src/app.po.ts
+++ project/meandemo/e2e/src/app.po.ts
@@ -10,35 +10,33 @@ export class AppPage {
   }
 
   async createEntry() {
-    var EC = protractor.ExpectedConditions;
+    const EC = protractor.ExpectedConditions;
     await browser.wait(EC.presenceOf(element(by.id('addEntryLink'))), 5000, 'Element taking too long to appear in the DOM');
-    element(by.id('addEntryLink')).click();  
-    await browser.wait(EC.presenceOf(element(by.css("input[formControlName=URL]"))), 5000, 'Element taking too long to appear in the DOM');
-    await element(by.css("input[formControlName=URL]")).sendKeys('https://github.com/jhuopensource/meandemo');
-    await element(by.css("input[formControlName=title]")).sendKeys('MEAN Demo');
-    await element(by.css("input[formControlName=author]")).sendKeys('Mike Sellers and Ali Soylu');
-    await element(by.css("textarea[formControlName=description]")).sendKeys('One MEAN (Mongo, Express, Angular, Node.js) CRUD on Docker Demo App.');
-    await element(by.id('save')).click();  
+    element(by.id('addEntryLink')).click();
+    await browser.wait(EC.presenceOf(element(by.css('input[formControlName=URL]'))), 5000, 'Element taking too long to appear in the DOM');
+    await element(by.css('input[formControlName=URL]')).sendKeys('https://github.com/jhuopensource/meandemo');
+    await element(by.css('input[formControlName=title]')).sendKeys('MEAN Demo');
+    await element(by.css('input[formControlName=author]')).sendKeys('Mike Sellers and Ali Soylu');
+    await element(by.css('textarea[formControlName=description]')).sendKeys('One MEAN CRUD on Docker Demo App.');
+    await element(by.id('save')).click();
     await element(by.id('delete')).click();
     return;
   }
 
   async editEntry() {
-    var EC = protractor.ExpectedConditions;
+    const EC = protractor.ExpectedConditions;
     await browser.wait(EC.presenceOf(element(by.id('addEntryLink'))), 5000, 'Element taking too long to appear in the DOM');
-    element(by.id('addEntryLink')).click();  
-    await browser.wait(EC.presenceOf(element(by.css("input[formControlName=URL]"))), 5000, 'Element taking too long to appear in the DOM');
-    await element(by.css("input[formControlName=URL]")).sendKeys('https://github.com/jhuopensource/meandemo');
-    await element(by.css("input[formControlName=title]")).sendKeys('MEAN Demo');
-    await element(by.css("input[formControlName=author]")).sendKeys('Mike Sellers and Ali Soylu');
-    await element(by.css("textarea[formControlName=description]")).sendKeys('One MEAN (Mongo, Express, Angular, Node.js) CRUD on Docker Demo App.');
-    await element(by.id('save')).click();  
+    element(by.id('addEntryLink')).click();
+    await browser.wait(EC.presenceOf(element(by.css('input[formControlName=URL]'))), 5000, 'Element taking too long to appear in the DOM');
+    await element(by.css('input[formControlName=URL]')).sendKeys('https://github.com/jhuopensource/meandemo');
+    await element(by.css('input[formControlName=title]')).sendKeys('MEAN Demo');
+    await element(by.css('input[formControlName=author]')).sendKeys('Mike Sellers and Ali Soylu');
+    await element(by.css('textarea[formControlName=description]')).sendKeys('One MEAN CRUD on Docker Demo App.');
+    await element(by.id('save')).click();
     await element(by.id('edit')).click();
-    await element(by.css("textarea[formControlName=description]")).sendKeys(' Was just CI/CD Pipieline Integrated.');
-    await element(by.id('save')).click();  
+    await element(by.css('textarea[formControlName=description]')).sendKeys(' CI/CD Pipieline Integrated.');
+    await element(by.id('save')).click();
     await element(by.id('delete')).click();
     return;
   }
-
-  
 }
