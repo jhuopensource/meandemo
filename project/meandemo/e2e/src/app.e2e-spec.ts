5f66f6354c770c30867040afb172141197131f94
mseller4 <mike.sellers@jhu.edu>
2019-05-24 12:01:55 -0400

    Selenium test updates to incorporate data validation

--- project/meandemo/e2e/src/app.e2e-spec.ts
+++ project/meandemo/e2e/src/app.e2e-spec.ts
@@ -8,12 +8,12 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('It should display the title welcome message.', async () => {
+  it('It should display the title welcome message. - Page Renders', async () => {
     await page.navigateTo();
     expect(await page.getParagraphText()).toEqual('One MEAN CRUD on Docker Demo');
   });
 
-  it('It should allow form data entry.', async () => {
+  it('It should allow form data entry.             - Database accepts record create, read, update and delete operations.', async () => {
     await page.navigateTo();
     await page.createEntry();
     await page.editEntry();
